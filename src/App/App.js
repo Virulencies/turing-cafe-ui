50499d27eb8ec7f175007c49b79b76060a320197
Theo McCray <tlmccraycray@gmail.com>
2024-04-09 10:44:58 -0600

    Add forms and form submission handelers

--- src/App/App.js
+++ src/App/App.js
@@ -20,13 +20,63 @@ function App() {
       .catch(error => console.error('Error fetching data: ', error));
   }, []);
 
+//form submission handlers
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData, //spread/rest here
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setReservations([...reservations, formData]);//s/r
+    setFormData({
+      name: '',
+      date: '',
+      time: '',
+      number: '',
+    });
+  };
+
 
 
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
-        {/* Form will go here in Iteration 2 */}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            name="date"
+            value={formData.date}
+            onChange={handleInputChange}
+            placeholder="Date (mm/dd)"
+          />
+          <input
+            type="text"
+            name="time"
+            value={formData.time}
+            onChange={handleInputChange}
+            placeholder="Time"
+          />
+          <input
+            type="number"
+            name="number"
+            value={formData.number}
+            onChange={handleInputChange}
+            placeholder="Number of Guests"
+          />
+          <button type="submit">Make Reservation</button>
+        </form>
       </div>
       <div className='resy-container'>
         {reservations.map((reservation, index) => (
